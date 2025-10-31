eafc1ce8de5027b0686861da5c2bdac5639e1f2d
commander-error404 <python3.go.c@gmail.com>
2025-10-31 01:37:41 -0700

    add settings.py with MySQL connection config and debug flag

---
+++ settings.py
@@ -0,0 +1,13 @@
+
+DEBUG: bool = None
+
+
+'''MySQL connection settings'''
+DB_HOST: list = [
+    'localhost',   # MySQL server address
+    'root',        # Username
+    '85200258'     # User password
+]
+
+
+ALLOWED_HOSTS: list = ['']
