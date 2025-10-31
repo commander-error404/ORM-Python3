1a47fd70e1e4ccaec09694c2da813385a2fb2fec
commander-error404 <python3.go.c@gmail.com>
2025-10-31 01:31:26 -0700

    implement identifier validation for table and column names add SQL injection prevention with table/column validation validate table and column names using isidentifier()

--- manager.py
+++ manager.py
@@ -1,7 +1,7 @@
 from contextlib import contextmanager
 import mysql.connector
 from mysql.connector import Error
-
+from settings import DB_HOST
 class DB_Manager():
     def __init__(self):
         pass
@@ -19,7 +19,7 @@ class DB_Manager():
             return self.db
 
 conn = DB_Manager.Connection()
-conn.connection(['localhost', 'root', '85200258'])
+conn.connection(DB_HOST)
 
 
 
@@ -30,6 +30,21 @@ class Model():
     def __init__(self, db):
         self.db = db
 
+    @staticmethod
+    def disable_foreign_key_checks(db):
+        """Disables foreign key checking""" 
+        with db.cursor() as cursor:
+            cursor.execute("SET FOREIGN_KEY_CHECKS=0")
+        db.commit()
+#region OFF
+    @staticmethod
+    def enable_foreign_key_checks(db):
+
+        """Enables foreign key checking""" 
+        with db.cursor() as cursor:
+            cursor.execute("SET FOREIGN_KEY_CHECKS=1")
+        db.commit()
+#region ON
     @contextmanager
     def get_cursor(self):
         db_connection = self.db.cursor(dictionary=True)
@@ -54,6 +69,26 @@ class Model():
         with self.get_cursor() as cursor:
             cursor.execute(sql, params)
             return cursor.fetchall()
+        
+    def add_values(self, columns: tuple[str], values: tuple):
+        if not self.__tablename__.isidentifier():
+            raise ValueError(f"Invalid table name: {self.__tablename__}")
+        
+        for col in columns:
+            if not col.isidentifier():
+                raise ValueError(f"Invalid column name: {col}")
+
+
+        cols = ', '.join(columns)
+        print("*****************simple join ", cols)
+        placeholders = ', '.join(['%s'] * len(values))
+        print("*****************advenced ", placeholders)
+        sql = f'INSERT INTO {self.__tablename__} ({cols}) VALUES ({placeholders})'
+
+        with self.get_cursor() as cursor:
+            cursor.execute(sql, values)
+            self.db.commit()
+            return 'ok'
 
     def create_table(self):
         if not self.__tablename__.isidentifier():
@@ -62,43 +97,152 @@ class Model():
         if not self.__fields__:
             raise ValueError("No fields specified")
 
-        drop_command = f"DROP TABLE IF EXISTS `{self.__tablename__}`"
-
-        # `id` VARCHAR
-        cols_sql = ', '.join(f"`{name_col}` {self.__fields__[name_col].sql_formate()}" for name_col in self.__fields__.keys())
-        create_sql = f'CREATE TABLE {self.__tablename__} ({cols_sql})'
+        # `personId` INT
+        cols_sql = ', '.join(f"`{name_col}` {self.__fields__[name_col].sql_format(name_col)}" for name_col in self.__fields__.keys())
+        create_sql = f'CREATE TABLE IF NOT EXISTS `{self.__tablename__}` ({cols_sql})'
+        print(create_sql)
         try:
             with self.get_cursor() as cursor:
-                cursor.execute(drop_command)
                 cursor.execute(create_sql)
             self.db.commit()
             print(f"{self.__tablename__}: created successfully!")
         except Error as er:
             print(f"Error working with MySQL: {er}")
     
+    def delete_table():
+        pass
+
+    def delete_values():
+        pass
+
 
     class CharField:
-        def __init__(self, max_len:int=None, null:bool=True):
-            self.max_len=max_len
+        def __init__(
+            self,
+            max_length: int=250,
+            null: bool=False, 
+            unique: bool=False,
+            primary_key: bool=False,
+            help_text: str=''
+        ):
+            self.max_length=max_length
             self.null=null
-        
-        def sql_formate(self):
-            return f'VARCHAR {self.max_len}'
-        
+            self.unique=unique
+            self.primary_key=primary_key
+            self.help_text=help_text
+
+        def sql_format(self, name_col: str):
+            null_str='' if self.null else 'NOT NULL'
+            unique_str='UNIQUE'if self.unique== True else''
+            pk_str='PRIMARY KEY' if self.primary_key == True else''
+
+            sql = ' '.join(filter(None, [null_str, unique_str, pk_str]))
+            print(f'[+]Column --[{name_col}] {sql}')
+            return f'VARCHAR({self.max_length}) {sql}'.strip()
+
+
+    class IntegerField:
+        def __init__(
+            self,
+            null: bool=False, 
+            unique: bool=False,
+            primary_key: bool=False,
+            auto_increment: bool=False,
+            help_text: str=''
+        ):
+            self.null=null
+            self.unique=unique
+            self.primary_key=primary_key
+            self.help_text=help_text
+            self.auto_increment = auto_increment
+
+
+        def sql_format(self, name_col: str):
+            null_str='' if self.null else 'NOT NULL'
+            unique_str='UNIQUE'if self.unique== True else''
+            pk_str='PRIMARY KEY' if self.primary_key == True else''
+            auto_increment = 'AUTO_INCREMENT' if self.auto_increment else ''
+
+            sql = ' '.join(filter(None, [auto_increment, null_str, unique_str, pk_str]))
+            print(f'[+]Column --[{name_col}] INT {sql}')
+            return f'INT {sql}'.strip()
+
+    class ImageField():
+        def __init__(
+                self, 
+                upload_to: str = '/', 
+                blank: bool = True, 
+                unique: bool=True, 
+                max_size: int = 50
+        ):
+            self.upload_to=upload_to
+            self.blank=blank
+            self.unique=unique
+            self.max_size=max_size
+
+
+        def sql_format(self, name_col: str):
+            null_str='' if self.blank else 'NOT NULL'
+            unique_str='UNIQUE'if self.unique== True else''
+
+            sql = ' '.join(filter(None, [null_str, unique_str]))
+            print(f'[+]Column --[{name_col}] {sql}')
+            return f'VARCHAR(250) {sql}'.strip()
+
+
+
+    class ForeignKey:
+        def __init__(self, model, CASCADE=False, reference:str='id'):
+            self.model=model
+            self.CASCADE=CASCADE
+            self.reference=reference
+
+        def sql_format(self, name_col: str):
+            cascade_sql = 'ON DELETE CASCADE' if self.CASCADE else 'ON DELETE RESTRICT'
+            
+            ref = self.reference  # id
+            model = self.model  # UserModel
+            field = model.__fields__[self.reference]
+            print("+++++++++", field)
+            
+            TYPE='INT' if isinstance(field, Model.IntegerField) else 'VARCHAR(11)'
+
+            return (
+                f'{TYPE}, FOREIGN KEY (`{name_col}`) '
+                f'REFERENCES `{self.model.__tablename__}` (`{self.reference}`) {cascade_sql}'
+            )
 
 class UserModel(Model):
-    __tablename__ = "users"
-    __fields__ = {
-        'id': Model.CharField(max_len=250),
-        'name': Model.CharField(max_len=200),
+   __tablename__ = 'user'
+   __fields__ = {
+        'id': Model.IntegerField(primary_key=True, auto_increment=True, unique=True),
+        'first_name':Model.CharField(max_length=150, help_text='User name'),
+        'last_name':Model.CharField(max_length=150, help_text='User name'),
     }
 
-userModel = UserModel(conn.db)
-userModel.create_table( (("PersonID", "int"),))
+# class Car(Model):
+#     __tablename__ = "Car"
+
+#     __fields__ = {
+#         'car_brand':Model.CharField(max_length=30),
+#         'car_type': Model.CharField(max_length=30),
+#         'owner': Model.ForeignKey(UserModel, CASCADE=True, reference="id"),
+#         'image': Model.ImageField(upload_to='/data', blank=True, unique=False,)
+#     }
 
 
+Model.disable_foreign_key_checks(conn.db)
 
-    # fields = {
-    #     'id': Column(Itnteger),
+model = UserModel(conn.db)
+# model.create_table()
+# model.add_values(
+#     columns=('name',),
+#     values=('Doe',)
+# )
+a = model.get_values(('id', 'first_name','last_name'))
+print(a)
+# model = Car(conn.db)
+# model.create_table()
 
-    # }
+
+Model.enable_foreign_key_checks(conn.db)
